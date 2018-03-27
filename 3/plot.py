50076771bf1ef836ba07e08da7b0e42676b94c1a
Sebastian Lütge <sebastian.luetge@tu-dortmund.de>
2018-03-27 19:28:20 +0200

    Habe etwas an Uebung Nr. 3 herum gepspielt :) texe meins dann auch mal so nice und adde das bei Zeiten

--- 3/plot.py
+++ 3/plot.py
@@ -1,8 +1,34 @@
+import matplotlib
+matplotlib.use('pgf')
 import matplotlib.pyplot as plt
 import numpy as np
+matplotlib.rcParams.update({
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+    'pgf.texsystem': 'lualatex',
+    'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}'
+})
 from uncertainties import ufloat
+import matplotlib.axes as Axes
 
 x = np.linspace(-10,10)
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('center')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.xlim(-6, 6)
+plt.ylim(-6, 6)
+plt.grid()
+plt.legend()
+plt.tight_layout(pad=0)
 
 plt.plot(x, -5*x+1, 'b-', label='f(x)')
 plt.plot(0, 1, 'bx')
@@ -20,12 +46,6 @@ plt.plot(-5/11, 36/11, 'rx', label=r'$\symup{S}_\text{f,g}$')
 plt.plot(3/7,-8/7, 'kx', label=r'$\symup{S}_\text{f,h}$')
 plt.plot(25/13, 24/13, 'mx', label=r'$\symup{S}_\text{g,h}$')
 
-plt.xlabel('x')
-plt.ylabel('y')
-plt.xlim(-6, 6)
-plt.ylim(-6, 6)
-plt.grid()
-plt.legend()
-plt.tight_layout(pad=0)
 plt.savefig('build/plot.pdf', bbox_inches='tight', pad_inches=0)
 plt.clf()
+
