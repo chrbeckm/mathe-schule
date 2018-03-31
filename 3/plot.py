dcdfcbd010324e15f660768f862560668e540606
Christian Beckmann <christian.beckmann@tu-dortmund.de>
2018-03-31 11:54:29 +0200

    3 leicht umgeschrieben

--- 3/plot.py
+++ 3/plot.py
@@ -9,7 +9,7 @@ matplotlib.rcParams.update({
     'pgf.texsystem': 'lualatex',
     'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}'
 })
-from uncertainties import ufloat
+#from uncertainties import ufloat
 import matplotlib.axes as Axes
 
 x = np.linspace(-10,10)
@@ -27,7 +27,6 @@ plt.ylabel('y')
 plt.xlim(-6, 6)
 plt.ylim(-6, 6)
 plt.grid()
-plt.legend()
 plt.tight_layout(pad=0)
 
 plt.plot(x, -5*x+1, 'b-', label='f(x)')
@@ -45,7 +44,7 @@ plt.plot(1, 0, 'yx')
 plt.plot(-5/11, 36/11, 'rx', label=r'$\symup{S}_\text{f,g}$')
 plt.plot(3/7,-8/7, 'kx', label=r'$\symup{S}_\text{f,h}$')
 plt.plot(25/13, 24/13, 'mx', label=r'$\symup{S}_\text{g,h}$')
+plt.legend()
 
 plt.savefig('build/plot.pdf', bbox_inches='tight', pad_inches=0)
 plt.clf()
-
