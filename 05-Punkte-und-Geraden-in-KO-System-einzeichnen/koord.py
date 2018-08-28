93f74a819e929cfbdba9f79644c384fc50a5a9d1
Christian Beckmann <christian.beckmann@udo.edu>
2018-08-28 12:39:00 +0200

    Makefiles verbessert, Ordner umbenannt

--- 05-Punkte-und-Geraden-in-KO-System-einzeichnen/koord.py
+++ 05-Punkte-und-Geraden-in-KO-System-einzeichnen/koord.py
@@ -1,30 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-ticks = np.linspace(-5,5,11)
+ticks = np.linspace(-5, 5, 11)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.xticks(ticks)
 plt.yticks(ticks)
-plt.xlim(-5.5,5.5)
-plt.ylim(-5.5,5.5)
+plt.xlim(-5.5, 5.5)
+plt.ylim(-5.5, 5.5)
 plt.grid()
 plt.tight_layout()
 plt.savefig('build/koordleer.pdf')
 plt.clf()
 
-ticks = np.linspace(-5,5,11)
+ticks = np.linspace(-5, 5, 11)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.xticks(ticks)
 plt.yticks(ticks)
-plt.xlim(-5.5,5.5)
-plt.ylim(-5.5,5.5)
+plt.xlim(-5.5, 5.5)
+plt.ylim(-5.5, 5.5)
 plt.grid()
-x = [-5,0,3,-1, 4,-2, 0,-4]
-y = [ 0,4,2, 4,-4,-3,-1, 2]
+x = [-5, 0, 3, -1,  4, -2,  0, -4]
+y = [ 0, 4, 2,  4, -4, -3, -1,  2]
 for i in range(len(x)):
-    plt.plot(x[i], y[i], 'x', label=i+1)
+    plt.plot(x[i], y[i], 'x', label = i+1)
 plt.legend()
 plt.tight_layout()
 plt.savefig('build/koordpunkte.pdf')
@@ -33,18 +33,18 @@ plt.clf()
 def linear(x,m,b):
     return m*x+b
 
-x = np.linspace(-5.5,5.5)
+x = np.linspace(-5.5, 5.5)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.xticks(ticks)
 plt.yticks(ticks)
-plt.xlim(-5.5,5.5)
-plt.ylim(-5.5,5.5)
+plt.xlim(-5.5, 5.5)
+plt.ylim(-5.5, 5.5)
 plt.grid()
-m = [1,0.8,-0.5,-1,0]
-b = [0,  4, 3.5,-4,4]
+m = [ 1, 0.8, -0.5, -1, 0]
+b = [ 0,   4,  3.5, -4, 4]
 for i in range(len(m)):
-    plt.plot(x, linear(x,m[i],b[i]), '-', label=i+1)
+    plt.plot(x, linear(x, m[i], b[i]), '-', label=i+1)
 plt.legend()
 plt.tight_layout()
 plt.savefig('build/koordgeraden.pdf')
